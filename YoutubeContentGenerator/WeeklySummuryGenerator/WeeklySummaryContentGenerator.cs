0c291c7943488f7f016c6e50773285ebe4cb529a
Maciej Wyrodek <32420051+mwyrodek@users.noreply.github.com>
2021-02-06 14:36:46 +0100

    removed stry file

--- YoutubeContentGenerator/WeeklySummuryGenerator/WeeklySummaryContentGenerator.cs
+++ YoutubeContentGenerator/WeeklySummuryGenerator/WeeklySummaryContentGenerator.cs
@@ -18,7 +18,7 @@ Since my channel is in polish for your conviniece I am [weekly sharing here](htt
 Now Let’s drink our ITea!
 ";
 
-        private static string TittleLine = "## ITea Morning #";
+        private static string TitleLine = "## ITea Morning #";
 
         private static string footer = "That is all for today see you next week!";
 
@@ -33,7 +33,7 @@ Now Let’s drink our ITea!
         content.AppendLine(Foreword);
             foreach (var episode in episodes)
             {
-                content.AppendLine($"{TittleLine}{episode.EpisodeNum}");
+                content.AppendLine($"{TitleLine}{episode.EpisodeNum}");
                 foreach (var article in episode.Articles)
                 {
                     content.AppendLine($"• [{article.Title}]({article.Link})");
