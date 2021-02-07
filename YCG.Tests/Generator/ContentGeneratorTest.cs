167dbc180fdbb3af9ce0d0b34e8c094be12cd4fa
Maciej Wyrodek <32420051+mwyrodek@users.noreply.github.com>
2021-02-07 21:37:36 +0100

    test for ContentGenerator a little overkill

---
+++ YCG.Tests/Generator/ContentGeneratorTest.cs
@@ -0,0 +1,38 @@
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Moq;
+using NUnit.Framework;
+using YoutubeContentGenerator;
+using YoutubeContentGenerator.Engine;
+using YoutubeContentGenerator.WeeklySummuryGenerator;
+
+namespace YCG.Tests.Generator
+{
+    [TestFixture]
+    public class ContentGeneratorTest
+    {
+        
+        private IFixture fixture;
+        private ContentGenerator sut;
+        
+
+        [SetUp]
+        public void Setup()
+        {
+            fixture = new Fixture().Customize(new AutoMoqCustomization());
+        }
+
+        [Test]
+        public void HappyPath_EveryThinkWorks()
+        {
+            var engine = fixture.Freeze<Mock<IEngine>>();
+            sut = fixture.Create<ContentGenerator>();
+            sut.Run();
+            
+            engine.Verify(e=>e.LoadData(),Times.Once);
+            engine.Verify(e=>e.GenerateLinks(),Times.Once);
+            engine.Verify(e=>e.GenerateDescription(),Times.Once);
+            engine.Verify(e=>e.GenerateWeekSummary(),Times.Once);
+        }
+    }
+}
--- YoutubeContentGenerator/ContentGenerator.cs
+++ YoutubeContentGenerator/ContentGenerator.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using YoutubeContentGenerator.Engine;
 
+[assembly:InternalsVisibleTo("YCG.Tests")]
 namespace YoutubeContentGenerator
 {
     public class ContentGenerator
